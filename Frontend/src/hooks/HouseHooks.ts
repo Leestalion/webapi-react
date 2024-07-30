a9fa8026e1c00513085e867e3d0a8d1ca2be21b4
Leestalion <lemarrecthomas@gmail.com>
2024-07-30 19:16:53 +0200

    first commit

---
+++ Frontend/src/hooks/HouseHooks.ts
@@ -0,0 +1,13 @@
+import config from "../config";
+import { House } from "../types/house";
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+
+const useFetchHouses = () => {
+    return useQuery<House[], AxiosError>({
+        queryKey: ["houses"],
+        queryFn: () => axios.get(`${config.baseApiUrl}/houses`).then((resp) => resp.data),
+    });
+}
+
+export default useFetchHouses;
---
+++ Frontend/src/main/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from "./Header"
+import HouseList from "../house/HouseList";
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Header subtitle="Providing houses all over the world" />
+      <HouseList />
+    </div>
+  )
+}
+
+export default App
